f276021a4fda2a7a1b55001b235a86d6b591a4f5
Alex Van Camp <email@alexvan.camp>
2016-06-26 09:46:49 -0500

    morning cleanup

---
+++ src/elements/toth-staff/toth-staff.js
@@ -0,0 +1,18 @@
+(function () {
+	'use strict';
+
+	Polymer({
+		is: 'toth-staff',
+
+		properties: {
+			name: String,
+			role: String,
+			handle: String,
+			picture: String,
+			email: String,
+			twitter: String,
+			youtube: String,
+			website: String
+		}
+	});
+})();

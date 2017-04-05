3227431a5edb731233ae25ad2554e48fbc0de961
Alex Van Camp <vancamp.alex@gmail.com>
2017-04-04 23:24:25 -0500

    fix(build): use eslint-es2015-nostrict preset
    Fixes the site on Safari, which blows up on the global "use strict";

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,6 @@ const vulcanize = require('gulp-vulcanize');
 const crisper = require('gulp-crisper');
 const uglify = require('gulp-uglify');
 const babel = require('gulp-babel');
-const sourcemaps = require('gulp-sourcemaps');
 const htmlmin = require('gulp-htmlmin');
 const imagemin = require('gulp-imagemin');
 const clean = require('gulp-clean');
@@ -39,13 +38,12 @@ gulp.task('minify', ['minify:js', 'minify:html', 'minify:images']);
 gulp.task('minify:js', ['vulcanize'], cb => {
 	pump([
 		gulp.src('build/**/*.js'),
-		sourcemaps.init(),
-		// Uglify can't handle ES2016, so we have to run it through babel first :/
-		babel({
-			presets: ['es2015']
-		}),
+
+		// We have to avoid the global 'use strict'; that babel adds, because it breaks the web animations polyfill:
+		// https://github.com/web-animations/web-animations-next/issues/402
+		// You'd think we could just use babili, but it chokes on some huge regex in Polymer so no go there either.
+		babel({presets: ['es2015-nostrict']}),
 		uglify(),
-		sourcemaps.write('./'),
 		gulp.dest('build')
 	], cb);
 });

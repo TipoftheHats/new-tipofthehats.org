e2aa2e71598fa202b2f578f045af8c417254d1f4
Alex Van Camp <vancamp.alex@gmail.com>
2017-04-04 23:49:38 -0500

    add echo to deploy script

--- deploy-to-production.sh
+++ deploy-to-production.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 ssh circleci@138.197.72.215 << EOF
 cd /home/lange/tipofthehats.org/
+echo "In dir, running git pull and friends"
 git pull && bower install && npm install && npm run build && pm2 restart tipofthehats.org
 EOF

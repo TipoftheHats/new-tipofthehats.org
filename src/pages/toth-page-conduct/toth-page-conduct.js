6a9a634c5d50595e88a26bee7f676d43b22ad757
Alex Van Camp <vancamp.alex@gmail.com>
2018-01-16 20:28:41 -0600

    feat: add code of conduct (#8)

---
+++ src/pages/toth-page-conduct/toth-page-conduct.js
@@ -0,0 +1,12 @@
+/**
+ * @customElement
+ * @polymer
+ * @extends Polymer.Element
+ */
+class TothPageConduct extends Polymer.Element {
+	static get is() {
+		return 'toth-page-conduct';
+	}
+}
+
+customElements.define(TothPageConduct.is, TothPageConduct);

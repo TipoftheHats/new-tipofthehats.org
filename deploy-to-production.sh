548a0a8af1dfa653762b234f3c89b45bb0f4cc7a
Alex Van Camp <vancamp.alex@gmail.com>
2018-07-25 09:31:03 -0500

    fix(deploy): prevent interactive prompt

--- deploy-to-production.sh
+++ deploy-to-production.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+ssh-keyscan -H 138.197.72.215 >> ~/.ssh/known_hosts
 ssh circleci@138.197.72.215 << EOF
 cd /home/lange/tipofthehats.org/
 echo "In dir, running git pull and friends"

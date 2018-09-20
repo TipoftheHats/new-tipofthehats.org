2187cfc20bcfd7395982af4e5dd5a086aeca270b
Alex Van Camp <vancamp.alex@gmail.com>
2018-09-20 14:41:33 -0500

    feat: add /sweepstakes page

---
+++ src/pages/toth-page-sweepstakes/toth-page-sweepstakes.js
@@ -0,0 +1,12 @@
+/**
+ * @customElement
+ * @polymer
+ * @extends Polymer.Element
+ */
+class TothPageSweepstakes extends Polymer.Element {
+	static get is() {
+		return 'toth-page-sweepstakes';
+	}
+}
+
+customElements.define(TothPageSweepstakes.is, TothPageSweepstakes);

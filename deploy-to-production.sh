d4f3ff19577d8d18df6c374890c824f3e87b600c
Alex Van Camp <vancamp.alex@gmail.com>
2018-09-28 11:58:09 -0500

    fix(deploy): update-env when restarting the server

--- deploy-to-production.sh
+++ deploy-to-production.sh
@@ -3,5 +3,5 @@ ssh-keyscan -H 138.197.72.215 >> ~/.ssh/known_hosts
 ssh circleci@138.197.72.215 << EOF
 cd /home/lange/tipofthehats.org/
 echo "In dir, running git pull and friends"
-git pull && bower install && npm install && npm run build && pm2 restart tipofthehats.org
+git pull && bower install && npm install && npm run build && pm2 restart --update-env tipofthehats.org
 EOF

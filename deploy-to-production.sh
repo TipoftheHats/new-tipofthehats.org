6e91b2e9c25ca9268ebf0fc2d0427485d1f85cbe
Alex Van Camp <vancamp.alex@gmail.com>
2018-09-28 15:22:12 -0500

    fix: update deploy script
    [skip ci]

--- deploy-to-production.sh
+++ deploy-to-production.sh
@@ -3,5 +3,7 @@ ssh-keyscan -H 138.197.72.215 >> ~/.ssh/known_hosts
 ssh circleci@138.197.72.215 << EOF
 cd /home/lange/tipofthehats.org/
 echo "In dir, running git pull and friends"
-git pull && bower install && npm install && npm run build && pm2 restart --update-env tipofthehats.org
+git pull && bower install && npm install && npm run build
+cd ..
+pm2 start pm2_tipofthehats.org.json --update-env
 EOF

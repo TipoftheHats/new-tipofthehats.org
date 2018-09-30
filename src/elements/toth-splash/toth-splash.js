c7c08182bab0130ec4e490d8e899b9a0e407213e
Alex Van Camp <vancamp.alex@gmail.com>
2018-09-30 14:41:40 -0500

    feat: add stream embed to splash

--- src/elements/toth-splash/toth-splash.js
+++ src/elements/toth-splash/toth-splash.js
@@ -1,12 +1,30 @@
-/**
- * @customElement
- * @polymer
- * @extends Polymer.Element
- */
-class TothSplash extends Polymer.Element {
-	static get is() {
-		return 'toth-splash';
-	}
-}
+(function () {
+	'use strict';
 
-customElements.define(TothSplash.is, TothSplash);
+	/**
+	 * @customElement
+	 * @polymer
+	 * @extends Polymer.Element
+	 */
+	class TothSplash extends Polymer.Element {
+		static get is() {
+			return 'toth-splash';
+		}
+
+		ready() {
+			super.ready();
+
+			if (window.Twitch) {
+				new window.Twitch.Embed(this.$['splash-content-stream'], { // eslint-disable-line no-new
+					width: '100%',
+					height: '100%',
+					channel: 'tipofthehats',
+					layout: 'video',
+					theme: 'dark'
+				});
+			}
+		}
+	}
+
+	customElements.define(TothSplash.is, TothSplash);
+})();

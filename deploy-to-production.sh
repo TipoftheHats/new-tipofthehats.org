a8d8001d6dbcf2d12260c68e363514c6aba3bc57
Alex Van Camp <vancamp.alex@gmail.com>
2018-10-13 12:57:43 -0500

    refactor: delete unused script
    [skip ci]

--- deploy-to-production.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-ssh-keyscan -H 138.197.72.215 >> ~/.ssh/known_hosts
-ssh circleci@138.197.72.215 << EOF
-cd /home/lange/tipofthehats.org/
-echo "In dir, running git pull and friends"
-git pull && bower install && npm install && npm run build
-cd ..
-pm2 start pm2_tipofthehats.org.json --update-env
-EOF
